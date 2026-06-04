6ed45441e60074bd8e973af7d1cc9fd6b7c6249d
Fiona <baiyang.feng@outlook.com>
2026-06-04 20:50:37 +0800

    chore: scaffold HarmonyOS HAR workspace

---
+++ flashcat-core/hvigorfile.ts
@@ -0,0 +1,6 @@
+import { harTasks } from '@ohos/hvigor-ohos-plugin';
+
+export default {
+  system: harTasks,  // HAR module lifecycle hooks; do not modify
+  plugins: []
+};
---
+++ hvigorfile.ts
@@ -0,0 +1,6 @@
+import { appTasks } from '@ohos/hvigor-ohos-plugin';
+
+export default {
+  system: appTasks,  // built-in project-level lifecycle hooks; do not modify
+  plugins: []        // custom plugins (e.g. compile-time AOP for network instrumentation) go here
+};
